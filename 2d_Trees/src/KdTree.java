277cd404175697107c396012868c9d06a7aaaf17
Qubo Song <sqb_007@hotmail.com>
2014-07-24 23:49:44 +0800

    updated

--- 2d_Trees/src/KdTree.java
+++ 2d_Trees/src/KdTree.java
@@ -39,73 +39,128 @@ public class KdTree {
         return size;
     }
 
-    private void insert(Point2D p, Node2D current, boolean isVertical) {
+    private boolean contains(Point2D p, Node2D current, boolean isVertical, boolean insert) {
         if (current.point.x() == p.x()
          && current.point.y() == p.y()) {
-            return;
+            return true;
         }
 
         if (isVertical) {
             if (p.x() < current.point.x()) {
                 if (current.lt == null) {
-                    current.lt = new Node2D(p);
+                    if (insert) {
+                        current.lt = new Node2D(p);
+                        size++;
+                    }
                 } else {
-                    insert(p, current.lt, HORIZONTAL);
+                    contains(p, current.lt, HORIZONTAL, insert);
                 }
             } else {
                 if (current.rt == null) {
-                    current.rt = new Node2D(p);
+                    if (insert) {
+                        current.rt = new Node2D(p);
+                        size++;
+                    }
                 } else {
-                    insert(p, current.rt, HORIZONTAL);
+                    contains(p, current.rt, HORIZONTAL, insert);
                 }
             }
         } else {
             if(p.y() < current.point.y()) {
                 if (current.lt == null) {
-                    current.lt = new Node2D(p);
+                    if (insert) {
+                        current.lt = new Node2D(p);
+                        size++;
+                    }
                 } else {
-                    insert(p, current.lt, VERTICAL);
+                    contains(p, current.lt, VERTICAL, insert);
                 }
             } else {
                 if (current.rt == null) {
-                    current.rt = new Node2D(p);
+                    if (insert) {
+                        current.rt = new Node2D(p);
+                        size++;
+                    }
                 } else {
-                    insert(p, current.rt, VERTICAL);
+                    contains(p, current.rt, VERTICAL, insert);
                 }
             }
         }
+
+        return false;
     }
 
     public void insert(Point2D p) {
     // add the point p to the set (if it is not already in the set)
-        Node2D current = root;
-        if (current == null) {
-            current = new Node2D(p);
+        if (root == null) {
+            root = new Node2D(p);
+            size++;
         } else {
-            insert(p, current, VERTICAL);
+            contains(p, root, VERTICAL, true);
         }
 
     }
 
     public boolean contains(Point2D p) {
     // does the set contain the point p?
-        return points.contains(p);
+        return root != null && contains(p, root, VERTICAL, false);
+    }
+
+    private void draw(Node2D current) {
+        if (current != null) {
+            draw(current.lt);
+            current.point.draw();
+            draw(current.rt);
+        }
     }
 
     public void draw() {
     // draw all of the points to standard draw
-        for (Point2D point : points) {
-            point.draw();
+        draw(root);
+    }
+
+    private void range(RectHV rect, Node2D current,
+                       boolean isVertical, SET<Point2D> points) {
+        if (current != null) {
+            if (isVertical) {
+                if (rect.xmax() < current.point.x()) {
+                    range(rect, current.lt, HORIZONTAL, points);
+                } else if (rect.xmin() > current.point.x()) {
+                    range(rect, current.rt, HORIZONTAL, points);
+                } else {
+                    points.add(current.point);
+                    range(rect, current.lt, HORIZONTAL, points);
+                    range(rect, current.rt, HORIZONTAL, points);
+                }
+            } else {
+                if (rect.ymax() < current.point.y()) {
+                    range(rect, current.lt, VERTICAL, points);
+                } else if (rect.ymin() > current.point.y()) {
+                    range(rect, current.rt, VERTICAL, points);
+                } else {
+                    points.add(current.point);
+                    range(rect, current.lt, VERTICAL, points);
+                    range(rect, current.rt, VERTICAL, points);
+                }
+            }
         }
     }
 
     public Iterable<Point2D> range(RectHV rect) {
     // all points in the set that are inside the rectangle
+        SET<Point2D> points = new SET<Point2D>();
+        range(rect, root, VERTICAL, points);
+        return points;
+    }
 
+    private Point2D nearest(Point2D p, Node2D current, boolean isVertical) {
+        if (current == null) return null;
     }
 
     public Point2D nearest(Point2D p) {
     // a nearest neighbor in the set to p; null if set is empty
+        return nearest(p, root, VERTICAL);
+
 
     }
 }
